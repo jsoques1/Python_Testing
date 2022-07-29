ff56d8dba4a1fcf47232e5baad116b7d9ed9377c
jsoques1 <joseph.soques@gmail.com>
2022-07-29 21:43:37 +0200

    add performance tests

--- tests/performance/locustfile.py
+++ tests/performance/locustfile.py
@@ -1,4 +1,4 @@
-from locust import HttpUser, task, between
+from locust import HttpUser, task
 from server import load_clubs, load_competitions, init_club_bookings
 
 
@@ -6,12 +6,17 @@ class TestPerformanceServer(HttpUser):
     competitions = load_competitions()
     clubs = load_clubs()
     bookings = init_club_bookings(clubs, competitions)
-    
     club = clubs[0]
     competition = competitions[0]
 
     def on_start(self):
         self.client.get('/')
+
+    def on_stop(self):
+        self.client.get("/logout")
+
+    @task
+    def connect(self):
         self.client.post('/showSummary', data={'email': self.club['email']})
 
     @task
