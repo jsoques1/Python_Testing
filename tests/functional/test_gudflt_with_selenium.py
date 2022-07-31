024b532ea0069ab4fb5ea8ed17b47e428ce781ca
jsoques1 <joseph.soques@gmail.com>
2022-07-31 15:31:30 +0200

    add more tests for selenium code

--- tests/functional/test_gudflt_with_selenium.py
+++ tests/functional/test_gudflt_with_selenium.py
@@ -6,7 +6,12 @@ from selenium.webdriver.support.wait import WebDriverWait
 from server import get_competition_club
 
 
-def book_places(competition_name, club_name, nb_of_places, status):
+def get_number(s):
+    number = s.strip(': ')[1]
+    return int(number)
+
+
+def book_places(competition_name, club_name, nb_of_places, status, msg):
     options = Options()
     # options.add_argument("headless")
 
@@ -30,6 +35,13 @@ def book_places(competition_name, club_name, nb_of_places, status):
         element = WebDriverWait(driver, 10).until(lambda x: x.find_element(By.ID, "show_summary"))
         element.click()
 
+        id_number_of_places = 'id_' + competition['name'].replace(' ', '_') + '_' + 'numberOfPlaces'
+        id_booked = 'id_' + competition['name'].replace(' ', '_') + '_' + 'booked'
+        points_available_before = WebDriverWait(driver, 10).until(lambda x: x.find_element(By.ID, "id_points"))
+        number_of_places_before = WebDriverWait(driver, 10).until(
+            lambda x: x.find_element(By.ID, id_number_of_places))
+        booked_before = WebDriverWait(driver, 10).until(lambda x: x.find_element(By.ID, id_booked))
+
         element = WebDriverWait(driver, 10).until(lambda x: x.find_element(By.ID, link_id))
         element.click()
 
@@ -39,8 +51,19 @@ def book_places(competition_name, club_name, nb_of_places, status):
         element = WebDriverWait(driver, 10).until(lambda x: x.find_element(By.ID, "booking"))
         element.click()
 
+        points_available_after = WebDriverWait(driver, 10).until(lambda x: x.find_element(By.ID, "id_points"))
+        number_of_places_after = WebDriverWait(driver, 10).until(
+            lambda x: x.find_element(By.ID, id_number_of_places))
+        booked_after = WebDriverWait(driver, 10).until(lambda x: x.find_element(By.ID, id_booked))
         element = WebDriverWait(driver, 10).until(lambda x: x.find_element(By.ID, status))
-        print(element.text)
+
+        if element.text == ' Success : Booking complete! ':
+            assert get_number(points_available_after.text) == get_number(points_available_before.text) - nb_of_places
+            assert get_number(number_of_places_after.text) == get_number(number_of_places_before.text) - nb_of_places
+            assert get_number(booked_after.text) == get_number(booked_before.text) - nb_of_places
+
+        if element.text != msg:
+            assert False
 
         element = WebDriverWait(driver, 10).until(lambda x: x.find_element(By.ID, "logout"))
         element.click()
@@ -55,20 +78,22 @@ def book_places(competition_name, club_name, nb_of_places, status):
 
 
 def test_normal_booking_1():
-    result = book_places('Spring Festival', 'Simply Lift', '1', 'success')
+    result = book_places('Spring Festival', 'Simply Lift', '1', 'success', 'Success : Booking complete!')
     assert result
 
 
 def test_normal_booking_2():
-    result = book_places('Spring Festival', 'Iron Temple', '4', 'success')
+    result = book_places('Spring Festival', 'Iron Temple', '4', 'success', 'Success : Booking complete!')
     assert result
 
 
 def test_wrong_booking_3():
-    result = book_places('Spring Festival', 'Simply Lift', '12', 'error')
+    result = book_places('Spring Festival', 'Simply Lift', '12', 'error',
+                         'Error : No more than 12 places can be purchased.')
     assert result
 
 
 def test_exception():
-    result = book_places('Dummy Festival', 'Dumb club', '', '')
+    result = book_places('Dummy Festival', 'Dumb club', '', 'error',
+                         'list index out of range')
     assert not result
